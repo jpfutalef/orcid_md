075e81bb3d8185ad07db5da1fcd3c6ae0fbb4cf2
Juan-Pablo Futalef <33015040+jpfutalef@users.noreply.github.com>
2024-03-11 13:38:18 +0100

    Cooler

--- orcid_record.py
+++ orcid_record.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import requests
 from pathlib import Path
-from rich import progress
+from rich.progress import Progress
 import json
 
-#%% ORCID data
+# %% ORCID data
 orcid_id = "0000-0002-7108-637X"
 ORCID_RECORD_API = "https://pub.orcid.org/v3.0/"
 
 json_loc = f"./ORCID-{orcid_id}.json"
 xlsx_loc = f"./ORCID-{orcid_id}.xlsx"
 md_loc = f"./ORCID-{orcid_id}.md"
-#%% Retrieve ORCID data
+# %% Retrieve ORCID data
 print("Retrieving ORCID entries from API...")
 
 response = requests.get(
@@ -33,7 +33,8 @@ print(f"     Retrieved ORCID record for {given_name} {family_name}...")
 print(f"     Found {len(orcid_record['activities-summary']['works']['group'])} works")
 print(f"     Saved to {json_loc}")
 
-#%% A function to fetch metadata for a given DOI
+
+# %% A function to fetch metadata for a given DOI
 def fetchmeta(doi, fmt="reference", **kwargs):
     """Fetch metadata for a given DOI.
 
@@ -87,91 +88,99 @@ def fetchmeta(doi, fmt="reference", **kwargs):
             out = r.text
     return out
 
-#%% Check if the xls file exists
+
+# %% Check if the xls file exists
 print("Checking if the xlsx file exists...")
 
 if Path(xlsx_loc).exists():
     print(f"    FOUND! {xlsx_loc} will be used to check if DOI is already in the list.")
-    df = pd.read_excel(xlsx_loc)
+    df = pd.read_excel(xlsx_loc, index_col=0)
     data = df.to_dict(orient="index")
 
 else:
     print(f"    NOT FOUND. New file will be created at {xlsx_loc}")
     data = {}
 
-#%% Obtain each work's metadata using the DOI
+# %% Obtain each work's metadata using the DOI
 print("Fetching reference data...")
-for iwork in progress.track(
-    orcid_record["activities-summary"]["works"]["group"], "Fetching reference data..."
-):
-    # The summary of the work
-    isummary = iwork["work-summary"][0]
 
-    # Extract the DOI
-    doi = None
-    for ii in isummary["external-ids"]["external-id"]:
-        if ii["external-id-type"] == "doi":
-            doi = ii["external-id-value"]
-            break
+with Progress() as progress:
+    bar = progress.add_task("Fetching DOI data...",
+                            total=len(orcid_record["activities-summary"]["works"]["group"]))
 
-    # If the DOI is not found, skip
-    if doi is None:
-        continue
+    for iwork in orcid_record["activities-summary"]["works"]["group"]:
+        # The summary of the work
+        isummary = iwork["work-summary"][0]
 
-    # If the DOI is already in the list, skip
-    if doi in data:
-        continue
+        # Extract the DOI
+        doi = None
+        for ii in isummary["external-ids"]["external-id"]:
+            if ii["external-id-type"] == "doi":
+                doi = ii["external-id-value"]
+                break
 
-    # Extract the data
-    try:
-        meta = fetchmeta(doi, fmt="dict")
-        doi_url = meta["URL"]
-        title = meta["title"]
-        references_count = meta["references-count"]
-        year = meta["issued"]["date-parts"][0][0]
-        url = meta["URL"]
+        # Update the progress bar. Ensure fixed width for the description
+        description = f"    Processing DOI: {doi}".ljust(50)
+        progress.update(bar, advance=1, description=description)
 
-        # Create authors list with links to their ORCIDs
-        authors = meta["author"]
-        autht = []
-        for author in authors:
-            name = f"{author['family']}, {author['given'][0]}."
-            if "holdgraf" in author["family"].lower():
-                name = f"**{name}**"
-            if "ORCID" in author:
-                autht.append(f"[{name}]({author['ORCID']})")
-            else:
-                autht.append(name)
-        autht = "; ".join(autht)
+        # If the DOI is not found, skip
+        if doi is None:
+            continue
 
-        journal = meta["publisher"]
+        # If the DOI is already in the list, skip
+        if doi in data:
+            continue
 
-        url_doi = url.split("//", 1)[-1]
-        reference = f"{autht} ({year}). **{title}**. {journal}. [{url_doi}]({url})"
+        # Extract the data
+        try:
+            meta = fetchmeta(doi, fmt="dict")
+            doi_url = meta["URL"]
+            title = meta["title"]
+            references_count = meta["references-count"]
+            year = meta["issued"]["date-parts"][0][0]
+            url = meta["URL"]
 
-        # Setup this entry
-        work_data = {
-            "title": title,
-            "authors": autht,
-            "year": year,
-            "journal": journal,
-            "citation_count": references_count,
-            "url_doi": url_doi,
-            "url": url,
-            "reference": reference
-        }
+            # Create authors list with links to their ORCIDs
+            authors = meta["author"]
+            autht = []
+            for author in authors:
+                name = f"{author['family']}, {author['given'][0]}."
+                if "holdgraf" in author["family"].lower():
+                    name = f"**{name}**"
+                if "ORCID" in author:
+                    autht.append(f"[{name}]({author['ORCID']})")
+                else:
+                    autht.append(name)
+            autht = "; ".join(autht)
 
-    except KeyError as e:
-        print(f"Failed to process DOI: {doi}")
+            journal = meta["publisher"]
 
-        work_data = {}
+            url_doi = url.split("//", 1)[-1]
+            reference = f"{autht} ({year}). **{title}**. {journal}. [{url_doi}]({url})"
 
-    # Add to the data
-    data[doi] = work_data
+            # Setup this entry
+            work_data = {
+                "title": title,
+                "authors": autht,
+                "year": year,
+                "journal": journal,
+                "citation_count": references_count,
+                "url_doi": url_doi,
+                "url": url,
+                "reference": reference
+            }
+
+        except KeyError as e:
+            print(f"Failed to process DOI: {doi}")
+
+            work_data = {}
+
+        # Add to the data
+        data[doi] = work_data
 
 print("Finished fetching reference data...")
 
-#%% Convert the data into a DataFrame
+# %% Convert the data into a DataFrame
 print("Converting data to a DataFrame...")
 df = pd.DataFrame(data).T
 
@@ -179,7 +188,7 @@ df = pd.DataFrame(data).T
 df.to_excel(xlsx_loc, index=False)
 print(f"    Saved to {xlsx_loc}")
 
-#%% Convert into a markdown string
+# %% Convert into a markdown string
 print("Converting data to markdown string...")
 md = []
 for year, items in df.groupby("year", sort=False):
